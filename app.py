5f7330ed6b5d7ff5dbd27a62385fbef8e0909f39
Vinicius Nogueira Oliveira <159262393+ViniciusNogueira2310@users.noreply.github.com>
2025-01-28 14:21:31 -0300

    add projeto v2

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/ola")
+def hello_world():
+    return "<p>Olá, mundo!</p>"
+
+
+# from model.interface import Interface
+
+# def main():
+#     Interface()
+
+# if __name__ == '__main__':
+#     main()
